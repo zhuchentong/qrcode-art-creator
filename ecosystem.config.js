37bde562db4eacf7ddbcc52082bd5c47cc2c4a12
zhuchentong <2037630@163.com>
2023-08-02 19:06:48 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,11 +22,11 @@ module.exports = {
       'user': 'root', // ssh 登陆服务器用户名
       'host': '121.37.181.45', // ssh 地址服务器domain/IP
       'ref': 'origin/master', // Git远程/分支
-      'repo': 'git@github.com:zhuchentong/chatgpt-0x.git', // git地址使用ssh地址
-      'path': '/data/chatgpt', // 项目存放服务器文件路径
+      'repo': 'git@github.com:zhuchentong/qrcode-art-creator.git', // git地址使用ssh地址
+      'path': '/data/qrcode-art-creator', // 项目存放服务器文件路径
       'ssh_options': 'StrictHostKeyChecking=no',
       'pre-deploy': 'git fetch --all',
-      'post-deploy': 'pnpm install && pnpm build && zx scripts/deploy/index.mjs', // 部署后的动作
+      'post-deploy': 'pnpm install && pnpm build && pm2 reload qrcode-art-creator', // 部署后的动作
     },
   },
 }
